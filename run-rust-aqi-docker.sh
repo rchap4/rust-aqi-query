5987eaa576ece40f41d1c06a8134a8e06118a471
R <37774865+rchap4@users.noreply.github.com>
2021-10-29 06:44:04 -0600

    Add new files to .gitignore

--- run-rust-aqi-docker.sh
+++ run-rust-aqi-docker.sh
@@ -1,7 +1,7 @@
 # Build and tag container first
 # Configure .env file with your API Key and Zipcode
 docker run \
-    --env-file .env \
+    --env-file .aqi-env \
     -p 3030:3030 \
     --rm \
     -d \
